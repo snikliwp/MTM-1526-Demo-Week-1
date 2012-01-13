ab4393dc5dd0bcb663286d50132a1bd1966470e7
snikliwp <wilk0146@algonquinlive.com>
2012-01-13 11:35:05 -0500

    break time

--- js/demo.js
+++ js/demo.js
@@ -2,3 +2,14 @@
 /*
 JavaScript multi-line comment
 */
+
+// alert pops up a little window with an OK button
+alert("What time is it?");
+
+// prompt is for user input, has a text field for the user to enter some text
+prompt("What is your name");
+
+var name = prompt("no really, what is your name?");
+console.log(name);
+
+document.write("Hello ", name);
